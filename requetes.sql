2051932b98d1fb08476a4ba0136555d2903afd35
Lily <112404773+Belywen@users.noreply.github.com>
2024-12-09 14:35:31 +0100

    maj

--- requetes.sql
+++ requetes.sql
@@ -18,14 +18,14 @@ INNER JOIN personne ON realisateur.id_personne = personne.id_personne
 ORDER BY nom ASC
 
 /*Requête d*/
-SELECT libelle_genre, COUNT(film.id_realisateur) AS libelle
+SELECT libelle_genre, COUNT(genre_film.id_film) AS libelle
 FROM genre_film
 INNER JOIN genre ON genre_film.id_genre = genre.id_genre
 INNER JOIN film ON genre_film.id_film = film.id_film
 GROUP BY genre.libelle_genre
 
 /*Requête e*/
-SELECT nom, prenom, COUNT(genre_film.id_film) AS nb_film
+SELECT nom, prenom, COUNT(film.id_realisateur) AS nb_film
 FROM film
 INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
 INNER JOIN personne ON realisateur.id_personne = personne.id_personne
@@ -41,7 +41,16 @@ INNER JOIN personne ON acteur.id_personne = personne.id_personne
 WHERE film.id_film = 1
 
 /*Requête g*/
+SELECT personne.id_personne, CONCAT(personne.nom, " ", personne.prenom) AS name_actor, titre, personnage, date_sortie_fr
+FROM casting
+INNER JOIN film ON casting.id_film = film.id_film
+INNER JOIN acteur ON casting.id_acteur = acteur.id_acteur
+INNER JOIN personne ON acteur.id_personne = personne.id_personne
+INNER JOIN role ON casting.id_role = role.id_role
+WHERE personne.id_personne = 2
+
 /*Requête h*/
+
 /*Requête i*/
 /*Requête j*/
 /*Requête k*/
