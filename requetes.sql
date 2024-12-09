19be247046cf5723ae23f93027d20cf92acbf6aa
Lily <112404773+Belywen@users.noreply.github.com>
2024-12-09 13:10:45 +0100

    maj

--- requetes.sql
+++ requetes.sql
@@ -1,8 +1,11 @@
 /*Requête a*/
-
-
+SELECT titre, YEAR(date_sortie_fr), round(duree / 60, 2), prenom, nom
+FROM film
+INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
+INNER JOIN personne ON realisateur.id_personne = personne.id_personne
 
 /*Requête b*/
+
 /*Requête c*/
 /*Requête d*/
 /*Requête e*/
