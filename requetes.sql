b5f8ba31079bedbfe085c031397bd83cd2ae0ecf
Lily <112404773+Belywen@users.noreply.github.com>
2024-12-09 13:27:56 +0100

    maj

--- requetes.sql
+++ requetes.sql
@@ -5,8 +5,18 @@ INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
 INNER JOIN personne ON realisateur.id_personne = personne.id_personne
 
 /*Requête b*/
+SELECT titre, round(duree / 60, 2)
+FROM film
+WHERE duree > 2.15 
+ORDER BY duree DESC
 
 /*Requête c*/
+SELECT titre, YEAR(date_sortie_fr), nom
+FROM film
+INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
+INNER JOIN personne ON realisateur.id_personne = personne.id_personne
+ORDER BY nom ASC
+
 /*Requête d*/
 /*Requête e*/
 /*Requête f*/
